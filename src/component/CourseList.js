1f8ae73638a2b341c4192a82399712d292118e26
jomon <jomosam@gmail.com>
2019-06-01 22:27:51 +0530

    Add course list

---
+++ src/component/CourseList.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react'
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import * as contentful from contentful
+import Course from '../component/Course'
+
+const SPACE_IS='';
+const ACCESS_TOKEN='';
